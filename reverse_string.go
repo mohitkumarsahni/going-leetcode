b085dd274ee8be64f103f0b4bd00c16d6b6f3c4f
Mohit Kumar Sahni <mohitkumarsahni@gmail.com>
2020-08-15 23:37:09 +0530

    LeetCode Day-1. Reverse Integer Problem.
    Day-1 of LeetCode & Day-1 in learning GoLang.

--- reverse_string.go
+++ reverse_string.go
@@ -4,37 +4,22 @@ import "fmt"
 
 func reverseInt(x int) int {
 	var temp int = 0
-	var ld int = 0
-	var temp_n int = x
 
 	if x > 2147483647 || x < -2147483648 {
 		return 0
 	}
-
-	if x < 0 {
-		temp_n = temp_n*-1
-	}
-
-	for temp_n>0 {
-		ld = temp_n%10
-		temp_n = temp_n/10
-		temp = temp*10+ld
-	}
-
-	if x < 0 {
-		temp = temp*-1
-	}
-
-	if temp > 2147483647 {
-		return 0
-	}else if temp < -2147483648 {
-		return 0
+	for x!=0 {
+		temp = (temp*10) + (x%10)
+		if temp > 2147483647 || temp < -2147483648{
+			return 0
+		}
+		x = x/10
 	}
 	return temp
 }
 
 func main()  {
-	var x int = -214748364
+	var x int = -214748367
 
 	fmt.Println("Reversing the Number: ",x)
 	fmt.Println(reverseInt(x))
